db900b4f7d93141fece80cf8651e133b84d11a86
Resul Ozdemir <zemirsd9@gmail.com>
2022-08-20 16:49:17 +0300

    Operatorler

---
+++ operator.cs
@@ -0,0 +1,67 @@
+Console.WriteLine("Hello, World!");
+
+// Atama ve işlemli atama
+int x = 3;
+int y = 3;
+y = y+2;
+
+Console.WriteLine("y");
+y += 2;
+Console.WriteLine("y");
+y /= 1;
+Console.WriteLine("y");
+x *= 2;
+Console.WriteLine("x");
+
+// Mantıksal operatörler
+// ||, &&, !
+Console.WriteLine("***** Mantıksal operatörler *****");
+
+bool isSuccess = true;
+bool isCompleted = false;
+
+if (isSuccess && isCompleted)
+    Console.WriteLine("Perfect!");
+
+if (isSuccess || isCompleted)
+    Console.WriteLine("Great!");
+
+if (isSuccess || !isCompleted)
+    Console.WriteLine("Fine!");
+
+// İlişkisel operatörler
+// <, >, <=, ==, >=, !=
+
+int a = 3;
+int b = 4;
+bool sonuc = a<b;
+Console.WriteLine(sonuc);
+sonuc = a>b;
+Console.WriteLine(sonuc);
+sonuc = a<=b;
+Console.WriteLine(sonuc);
+sonuc = a==b;
+Console.WriteLine(sonuc);
+sonuc = a>=b;
+Console.WriteLine(sonuc);
+sonuc = a!=b;
+Console.WriteLine(sonuc);
+
+// Aritmetik operatörler
+// /, *, +, -, ++, mod
+
+int sayi1 = 10;
+int sayi2 = 5;
+
+int sonuc1 = sayi1/sayi2;
+Console.WriteLine(sonuc1);
+sonuc1 = sayi1*sayi2;
+Console.WriteLine(sonuc1);
+sonuc1 = sayi1+sayi2;
+Console.WriteLine(sonuc1);
+sonuc1 = sayi1-sayi2;
+Console.WriteLine(sonuc1);
+sonuc1 = ++sayi1;
+Console.WriteLine(sonuc1);
+sonuc1 = 20%3;
+Console.WriteLine(sonuc1);
