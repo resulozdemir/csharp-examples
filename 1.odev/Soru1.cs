cc8e43b2c9b80b5b37b36a7f54b76d9a9cbe89e3
Resul Ozdemir <zemirsd9@gmail.com>
2022-08-25 02:47:33 +0300

    update

--- 1.odev/Soru1.cs
+++ 1.odev/Soru1.cs
@@ -7,17 +7,17 @@ namespace Soru1
         static void Main(string[] args)
         {
             Console.WriteLine("Lutfen bir sayi giriniz : ");
-            int adet = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            int[] dizi = new int[adet];
+            int[] dizi = new int[n];
 
-            for (int i = 0; i < adet; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.WriteLine((i+1) + ".Elemanı giriniz : ");
                 dizi[i] = Convert.ToInt32(Console.ReadLine());
             }
 
-            for (int i = 0; i < adet; i++)
+            for (int i = 0; i < n; i++)
             {
                 if(dizi[i]%2==0)
                     Console.WriteLine(dizi[i]);
